57a624719bd2c52b95110e36e9550e97ccd31e9d
BirdiD <diioula.doucoure@gmail.com>
2024-02-26 16:41:33 +0000

    add caching

--- Mistral/utils.py
+++ Mistral/utils.py
@@ -8,6 +8,7 @@ from transformers import (
 )
 from langchain.llms import HuggingFacePipeline
 
+cached_model = {}
 
 def build_llm_pipeline(model_name, max_new_tokens, temperature, repetition_penalty):
     """
@@ -20,32 +21,35 @@ def build_llm_pipeline(model_name, max_new_tokens, temperature, repetition_penal
     Returns:
         HuggingFacePipeline mistral llm object
     """
-    # Tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
+    llm = cached_model.get(model_name)
+    if llm is None: 
+        # Tokenizer
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, trust_remote_code=True)
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = "right"
 
-    # Set up quantization config
-    bnb_config = BitsAndBytesConfig(load_in_4bit=True,
-                                    bnb_4bit_use_double_quant=True,
-                                    bnb_4bit_quant_type="nf4",
-                                    bnb_4bit_compute_dtype=torch.bfloat16)
-    
-    # Load pre-trained config
-    model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                trust_remote_code=True,
-                                                device_map="auto",
-                                                quantization_config=bnb_config)
+        # Set up quantization config
+        bnb_config = BitsAndBytesConfig(load_in_4bit=True,
+                                        bnb_4bit_use_double_quant=True,
+                                        bnb_4bit_quant_type="nf4",
+                                        bnb_4bit_compute_dtype=torch.bfloat16)
+        
+        # Load pre-trained config
+        model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                    trust_remote_code=True,
+                                                    device_map="auto",
+                                                    quantization_config=bnb_config)
 
-    # Model pipeline
-    model_pipeline = pipeline(model=model,
-                            tokenizer=tokenizer,
-                            task="text-generation",
-                            temperature=temperature,
-                            repetition_penalty=repetition_penalty,
-                            max_new_tokens=max_new_tokens,
-                            do_sample = temperature > 0.0)                                           
+        # Model pipeline
+        model_pipeline = pipeline(model=model,
+                                tokenizer=tokenizer,
+                                task="text-generation",
+                                temperature=temperature,
+                                repetition_penalty=repetition_penalty,
+                                max_new_tokens=max_new_tokens,
+                                do_sample = temperature > 0.0)                                           
 
-    llm = HuggingFacePipeline(pipeline=model_pipeline)
+        llm = HuggingFacePipeline(pipeline=model_pipeline)
+        cached_model[model_name] = llm
 
     return llm
