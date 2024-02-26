79a7fde28d8967a577877d4419de615f40a37141
BirdiD <diioula.doucoure@gmail.com>
2024-02-26 22:05:48 +0000

    update .sh

--- CamemBert/run_training.sh
+++ CamemBert/run_training.sh
@@ -1,7 +1,7 @@
 python train.py --model_name_or_path="camembert/camembert-base" \
                 --data_folder_path="Data/Classeur1_catg_phrases.xlsx" \
                 --output_dir="output" \
-                --hub_model_id="DioulaD/classificateur-intention_camembert" \
-                --max_steps="100" \
-                --logging_steps="10" \
-                --save_steps="20"
+                --hub_model_id="DioulaD/classifier-camembert" \
+                --max_steps="400" \
+                --logging_steps="100" \
+                --save_steps="100"
