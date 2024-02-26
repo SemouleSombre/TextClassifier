feed0ce7f6fc372e7f3ac1675b030aaf5ab97518
BirdiD <diioula.doucoure@gmail.com>
2024-02-26 16:28:23 +0000

    update .sh file

--- Mistral/run.sh
+++ Mistral/run.sh
@@ -1,2 +1,2 @@
 python classifier.py --model_name='mistralai/Mistral-7B-Instruct-v0.2' \
-                     --sentence="Ferme automatiquement les portes à l'heure prévue."
+                     --sentence="Balance les robots pour trier tout ce bazar, s'il te plaît."
